428da5444f77c64082c41c8086f56f4b7ac4de8a
Marcus Crane <marcus@utf9k.net>
2020-10-31 17:23:56 +1300

    Added a dashboard app to handle user overview

--- astigmia/settings.py
+++ astigmia/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lobby.apps.LobbyConfig',
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- astigmia/urls.py
+++ astigmia/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('lobby.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
