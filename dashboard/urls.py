ddcc9756d55e8cae56e21f45938cf790aeb480ec
Marcus Crane <marcus@utf9k.net>
2020-11-07 11:54:11 +1300

    Switch hardcoded URLs for refs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='dashboard_index'),
     path('notifications', views.notifications, name='notifications')
 ]
--- lobby/urls.py
+++ lobby/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='lobby_index'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # TODO: Import password_change / password_reset routes with overrides to call MV API
