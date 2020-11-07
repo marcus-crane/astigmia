7635f3bbc322f699bd479527888c3219f14bfb37
Marcus Crane <marcus@utf9k.net>
2020-11-07 13:25:04 +1300

    Increase max length of notifications

--- dashboard/models.py
+++ dashboard/models.py
@@ -12,4 +12,4 @@ class Notification(models.Model):
     """
     id = models.CharField(primary_key=True, editable=False, max_length=40)
     created_at = models.DateTimeField()
-    message = models.CharField(max_length=200)
+    message = models.CharField(max_length=300)
